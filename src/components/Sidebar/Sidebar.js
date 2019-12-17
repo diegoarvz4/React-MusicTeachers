df6a6378c0a3cb06c18b5f0e57850ccce926bfd5
diegoarvz4 <darvz.gtz@gmail.com>
2019-12-17 14:23:04 -0600

    Styled Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
+import closeIcon from './close-icon.svg';
+import appoIcon from './appointment.svg';
+import teachersIcon from './teachers.svg';
 
 export default (props) => {
 
@@ -11,17 +14,19 @@ export default (props) => {
 
   return (
     <div className="sidebar">
-      <h1>{props.username}</h1>
-      <span onClick={props.hideSideBar}>Close</span>
+      <div className="sidebar-head">
+        <h1 className="sidebar-user">{props.username}</h1>
+        <img onClick={props.hideSideBar} src={closeIcon} />
+      </div>
       <hr />
-      <h2 onClick={props.hideSideBar}><Link to={{
+      <h2 onClick={props.hideSideBar}><Link className="sidebar-link" to={{
         pathname: '/appointments'
-      }}>Appointments</Link></h2>
-      <h2 onClick={props.hideSideBar}><Link to={{
+      }}><img src={appoIcon}/><span>Appointments</span></Link></h2>
+      <h2 onClick={props.hideSideBar}><Link className="sidebar-link" to={{
         pathname: '/'
-      }}>Search Music Teachers</Link></h2>
+      }}><img src={teachersIcon}/><span>Search Music Teachers</span></Link></h2>
       <hr />
-      <h2 onClick={handleLogout}>Logout</h2>
+      <h2 className="sidebar-link sidebar-link-logout" onClick={handleLogout}>Logout</h2>
 </div>
   )
 }
