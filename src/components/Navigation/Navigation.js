8bb8e481283c31e0c672a80705d744598fd9066f
diegoarvz4 <darvz.gtz@gmail.com>
2019-12-17 14:03:59 -0600

    Added Navbar Styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import menuIcon from './menu-icon.svg';
 
 export default (props) => {
   return (
     <nav>
       <ul>
-        <li onClick={props.showSideBar}> Ham</li>
-        <li>Logo</li>
+        <li onClick={props.showSideBar}><img src={menuIcon}/></li>
+        <li><span>musiclass</span></li>
       </ul>
     </nav>
   );
